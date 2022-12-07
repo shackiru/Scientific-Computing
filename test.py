3f2e7ac9c1d0387b9a3e385a5c897d6c5a716cc3
shaqqputra <84982248+shaqqputra@users.noreply.github.com>
2022-12-07 09:32:55 +0700

    no lie

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec  7 08:05:03 2022
-
-@author: bcamaster
-"""
-
-print("Hello World")
+from numpy import dot
+def gaussElimin(a, b):
+    n = len(b)
+    for k in range (0, n - 1):
+        for i in range (k + 1):
+            if a[i, k] != 0.0:
+                lam = a[i, k] / a[k, k]
+                a[i, k + 1: n] = a[i, k + 1: n] - lam*a[k, k + 1:n]
+                b[i] = b[i] - lam * b[k]
+    for k in range(n - 1, -1, -1):
+        b[k] = (b[k] - dot(a[k, k + 1:n],b[k + 1:n])) / a[k,k]
+        return b
+                
